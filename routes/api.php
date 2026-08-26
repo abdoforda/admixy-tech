1039f53506ee6220ccaf82c4cb6e7b0852e3a2d1
abdoforda <abdelrahmaan3@gmai.com>
2026-08-27 02:28:58 +0300

    a

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/webhook/deploy', [SiteController::class, 'deploy']);
+Route::get('/webhook/deploy', [SiteController::class, 'deploy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
