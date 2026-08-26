fbd107361f72668c82f9032ba895c76a18d5f4f1
abdoforda <abdelrahmaan3@gmai.com>
2026-08-27 02:27:20 +0300

    a

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// aaaaaaaaaaaaaaaaaaaaaaaaaaa
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
@@ -37,5 +38,3 @@ Route::get('/faq', function () {
 Route::get('/contact-us', function () {
     return view('contact');
 })->name('contact');
-
-Route::post('/webhook/deploy', [SiteController::class, 'deploy']);
